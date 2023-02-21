43a6d47b0e1b3cf8e93bd850d031716e5ef7a690
Shawn Benoit <benoitsd@gmail.com>
2023-02-20 21:52:29 -0600

    updates and experimentation

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,105 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.CurrentTimeLabel = new System.Windows.Forms.Label();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.exitButton = new System.Windows.Forms.Button();
-            this.fullScreenButton = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // CurrentTimeLabel
-            // 
-            this.CurrentTimeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			this.components = new System.ComponentModel.Container();
+			this.CurrentTimeLabel = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.exitButton = new System.Windows.Forms.Button();
+			this.fullScreenButton = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.EnterTimeLabel = new System.Windows.Forms.Button();
+			this.SelectMusicFolder = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// CurrentTimeLabel
+			// 
+			this.CurrentTimeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.CurrentTimeLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(38)))), ((int)(((byte)(58)))));
-            this.CurrentTimeLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.CurrentTimeLabel.Font = new System.Drawing.Font("Arial", 99.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CurrentTimeLabel.ForeColor = System.Drawing.Color.DarkGray;
-            this.CurrentTimeLabel.Location = new System.Drawing.Point(0, 390);
-            this.CurrentTimeLabel.Margin = new System.Windows.Forms.Padding(51);
-            this.CurrentTimeLabel.Name = "CurrentTimeLabel";
-            this.CurrentTimeLabel.Size = new System.Drawing.Size(933, 151);
-            this.CurrentTimeLabel.TabIndex = 0;
-            this.CurrentTimeLabel.Text = "30:00:00";
-            this.CurrentTimeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // timer1
-            // 
-            this.timer1.Interval = 1000;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // exitButton
-            // 
-            this.exitButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+			this.CurrentTimeLabel.AutoSize = true;
+			this.CurrentTimeLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(38)))), ((int)(((byte)(58)))));
+			this.CurrentTimeLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.CurrentTimeLabel.Font = new System.Drawing.Font("Arial", 99.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.CurrentTimeLabel.ForeColor = System.Drawing.Color.White;
+			this.CurrentTimeLabel.Location = new System.Drawing.Point(174, 386);
+			this.CurrentTimeLabel.Margin = new System.Windows.Forms.Padding(51);
+			this.CurrentTimeLabel.Name = "CurrentTimeLabel";
+			this.CurrentTimeLabel.Size = new System.Drawing.Size(597, 153);
+			this.CurrentTimeLabel.TabIndex = 0;
+			this.CurrentTimeLabel.Text = "00:00:00";
+			this.CurrentTimeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 1000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// exitButton
+			// 
+			this.exitButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.exitButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.exitButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(38)))), ((int)(((byte)(58)))));
-            this.exitButton.FlatAppearance.BorderColor = System.Drawing.SystemColors.HotTrack;
-            this.exitButton.FlatAppearance.BorderSize = 0;
-            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.exitButton.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.exitButton.ForeColor = System.Drawing.Color.DimGray;
-            this.exitButton.Location = new System.Drawing.Point(0, 549);
-            this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(933, 25);
-            this.exitButton.TabIndex = 1;
-            this.exitButton.Text = "Exit";
-            this.exitButton.UseVisualStyleBackColor = false;
-            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
-            // 
-            // fullScreenButton
-            // 
-            this.fullScreenButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+			this.exitButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.exitButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(38)))), ((int)(((byte)(58)))));
+			this.exitButton.FlatAppearance.BorderColor = System.Drawing.SystemColors.HotTrack;
+			this.exitButton.FlatAppearance.BorderSize = 0;
+			this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.exitButton.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.exitButton.ForeColor = System.Drawing.Color.DimGray;
+			this.exitButton.Location = new System.Drawing.Point(0, 549);
+			this.exitButton.Name = "exitButton";
+			this.exitButton.Size = new System.Drawing.Size(933, 25);
+			this.exitButton.TabIndex = 1;
+			this.exitButton.Text = "Exit";
+			this.exitButton.UseVisualStyleBackColor = false;
+			this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+			// 
+			// fullScreenButton
+			// 
+			this.fullScreenButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.fullScreenButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.fullScreenButton.FlatAppearance.BorderColor = System.Drawing.SystemColors.HotTrack;
-            this.fullScreenButton.FlatAppearance.BorderSize = 0;
-            this.fullScreenButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.fullScreenButton.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.fullScreenButton.ForeColor = System.Drawing.Color.DimGray;
-            this.fullScreenButton.Location = new System.Drawing.Point(0, 0);
-            this.fullScreenButton.Name = "fullScreenButton";
-            this.fullScreenButton.Size = new System.Drawing.Size(933, 22);
-            this.fullScreenButton.TabIndex = 2;
-            this.fullScreenButton.Text = "Fullscreen";
-            this.fullScreenButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.fullScreenButton.UseVisualStyleBackColor = true;
-            this.fullScreenButton.Click += new System.EventHandler(this.fullScreenButton_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(38)))), ((int)(((byte)(58)))));
-            this.pictureBox1.ErrorImage = global::Clock_and_Timer.Properties.Resources.Tellepsen_sharepoint_logo;
-            this.pictureBox1.Image = global::Clock_and_Timer.Properties.Resources.Tellepsen_sharepoint_logo;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 28);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(933, 363);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
-            // 
-            // Clock
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(38)))), ((int)(((byte)(58)))));
-            this.ClientSize = new System.Drawing.Size(933, 574);
-            this.Controls.Add(this.fullScreenButton);
-            this.Controls.Add(this.exitButton);
-            this.Controls.Add(this.CurrentTimeLabel);
-            this.Controls.Add(this.pictureBox1);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 128F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(51, 46, 51, 46);
-            this.Name = "Clock";
-            this.Text = "ClockandTimer";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
+			this.fullScreenButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.fullScreenButton.FlatAppearance.BorderColor = System.Drawing.SystemColors.HotTrack;
+			this.fullScreenButton.FlatAppearance.BorderSize = 0;
+			this.fullScreenButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.fullScreenButton.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.fullScreenButton.ForeColor = System.Drawing.Color.DimGray;
+			this.fullScreenButton.Location = new System.Drawing.Point(445, 0);
+			this.fullScreenButton.Name = "fullScreenButton";
+			this.fullScreenButton.Size = new System.Drawing.Size(488, 29);
+			this.fullScreenButton.TabIndex = 2;
+			this.fullScreenButton.Text = "Fullscreen";
+			this.fullScreenButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.fullScreenButton.UseVisualStyleBackColor = true;
+			this.fullScreenButton.Click += new System.EventHandler(this.fullScreenButton_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(38)))), ((int)(((byte)(58)))));
+			this.pictureBox1.ErrorImage = global::Clock_and_Timer.Properties.Resources.Tellepsen_sharepoint_logo;
+			this.pictureBox1.Image = global::Clock_and_Timer.Properties.Resources.Tellepsen_sharepoint_logo;
+			this.pictureBox1.Location = new System.Drawing.Point(0, 28);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(933, 357);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 3;
+			this.pictureBox1.TabStop = false;
+			// 
+			// EnterTimeLabel
+			// 
+			this.EnterTimeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.EnterTimeLabel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.EnterTimeLabel.FlatAppearance.BorderColor = System.Drawing.SystemColors.HotTrack;
+			this.EnterTimeLabel.FlatAppearance.BorderSize = 0;
+			this.EnterTimeLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.EnterTimeLabel.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.EnterTimeLabel.ForeColor = System.Drawing.Color.DimGray;
+			this.EnterTimeLabel.Location = new System.Drawing.Point(0, 0);
+			this.EnterTimeLabel.Name = "EnterTimeLabel";
+			this.EnterTimeLabel.Size = new System.Drawing.Size(67, 29);
+			this.EnterTimeLabel.TabIndex = 4;
+			this.EnterTimeLabel.Text = "Enter time";
+			this.EnterTimeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.EnterTimeLabel.UseVisualStyleBackColor = true;
+			// 
+			// SelectMusicFolder
+			// 
+			this.SelectMusicFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.SelectMusicFolder.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.SelectMusicFolder.FlatAppearance.BorderColor = System.Drawing.SystemColors.HotTrack;
+			this.SelectMusicFolder.FlatAppearance.BorderSize = 0;
+			this.SelectMusicFolder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.SelectMusicFolder.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.SelectMusicFolder.ForeColor = System.Drawing.Color.DimGray;
+			this.SelectMusicFolder.Location = new System.Drawing.Point(173, 0);
+			this.SelectMusicFolder.Name = "SelectMusicFolder";
+			this.SelectMusicFolder.Size = new System.Drawing.Size(134, 29);
+			this.SelectMusicFolder.TabIndex = 5;
+			this.SelectMusicFolder.Text = "Select Music";
+			this.SelectMusicFolder.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.SelectMusicFolder.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(38)))), ((int)(((byte)(58)))));
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.ForeColor = System.Drawing.Color.White;
+			this.textBox1.Location = new System.Drawing.Point(70, 6);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(0);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 19);
+			this.textBox1.TabIndex = 6;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// Clock
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(38)))), ((int)(((byte)(58)))));
+			this.ClientSize = new System.Drawing.Size(933, 574);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.SelectMusicFolder);
+			this.Controls.Add(this.EnterTimeLabel);
+			this.Controls.Add(this.fullScreenButton);
+			this.Controls.Add(this.exitButton);
+			this.Controls.Add(this.CurrentTimeLabel);
+			this.Controls.Add(this.pictureBox1);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 128F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.ForeColor = System.Drawing.SystemColors.Highlight;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Margin = new System.Windows.Forms.Padding(51, 46, 51, 46);
+			this.Name = "Clock";
+			this.Text = "ClockandTimer";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -137,6 +194,9 @@
         private System.Windows.Forms.Button exitButton;
         private System.Windows.Forms.Button fullScreenButton;
         private System.Windows.Forms.PictureBox pictureBox1;
-    }
+		private System.Windows.Forms.Button EnterTimeLabel;
+		private System.Windows.Forms.Button SelectMusicFolder;
+		private System.Windows.Forms.TextBox textBox1;
+	}
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,74 +1,76 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
 
 namespace Clock_and_Timer
 {
-    public partial class Clock : Form
-    {
-        public Clock()
-        {
-            InitializeComponent();
-            current_timer();
-        }
+	public partial class Clock : Form
+	{
+		public Clock()
+		{
+			InitializeComponent();
+			Current_timer();
+		}
 
-        private int duration = 30;
+		public void Current_timer()
+		{
+			int timeToCountDown;
+			timer1.Start();
+		}
 
-        public DateTime current_timer()
-        {
-            timer1 = new System.Windows.Forms.Timer();
-            timer1.Tick -= new EventHandler(count_down);
-            timer1.Interval = 1000;
-            timer1.Start();
-        }
+		public int timeSpan(int currentTime)
+		{
+			int startTime = currentTime;
+			int endTime = currentTime -= 1000;
+			int seconds = startTime - endTime;
+			return seconds;
+		}
 
-        public void count_down()
-        {
-            if (duration == 0)
-            {
-                timer1.Stop();
-            }
-            else if (duration > 0)
-            {
-                duration--;
-                CurrentTimeLabel.Text = duration.ToString();
-            }
-        }
+		public void Count_down(int duration)
+		{
+			if(duration == 0)
+			{
+				timer1.Stop();
+			}
+			else if(duration > 0)
+			{
+				duration--;
+				CurrentTimeLabel.Text = duration.ToString();
+			}
+		}
 
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            count_down();
-        }
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			Count_down();
+		}
 
+		private void fullScreenButton_Click(object sender, EventArgs e)
+		{
+			if(Clock.ActiveForm.WindowState == FormWindowState.Maximized)
+			{
+				fullScreenButton.Text = "Fullscreen";
+				Clock.ActiveForm.FormBorderStyle = FormBorderStyle.Sizable;
+				Clock.ActiveForm.WindowState = FormWindowState.Normal;
+			}
+			else
+			{
+				fullScreenButton.Text = "Restore";
+				Clock.ActiveForm.FormBorderStyle = FormBorderStyle.None;
+				Clock.ActiveForm.WindowState = FormWindowState.Maximized;
+			}
+		}
 
-        private void fullScreenButton_Click(object sender, EventArgs e)
-        {
-            if (Clock.ActiveForm.WindowState == FormWindowState.Maximized)
-            {
-                fullScreenButton.Text = "Fullscreen";
-                Clock.ActiveForm.FormBorderStyle = FormBorderStyle.Sizable;
-                Clock.ActiveForm.WindowState = FormWindowState.Normal;
-            }
-            else
-            {
-                fullScreenButton.Text = "Restore";
-                Clock.ActiveForm.FormBorderStyle = FormBorderStyle.None;
-                Clock.ActiveForm.WindowState = FormWindowState.Maximized;
-            }
-        }
+		private void exitButton_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
 
-        private void exitButton_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
-
-    }
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			if(textBox1 != null)
+			{
+				int duration = int.Parse(textBox1.Text) * 60;
+				timeSpan(duration);
+			}
+		}
+	}
 }
